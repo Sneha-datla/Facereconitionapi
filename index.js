f9ad5466e4841a1a117117ef1bfd7611b74637b3
Sneha-datla <datla.sneha1@gmail.com>
2025-07-04 18:03:55 +0530

    changes made

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const fs = require('fs');
-const pool = require('./db');
+const pool = require('../db');
 const { loadModels, getDescriptor, euclideanDistance } = require('./faceUtils');
 
 const app = express();
